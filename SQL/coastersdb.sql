24f36e6b7894e1af013f29a9037478f55c2f1fc2
Jason Smith <jasondsmith1@hotmail.com>
2022-11-13 11:30:54 -0500

    first commit

---
+++ SQL/coastersdb.sql
@@ -0,0 +1,114 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: rollercoasters
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `coasters`
+--
+
+DROP TABLE IF EXISTS `coasters`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `coasters` (
+  `coasterID` int NOT NULL,
+  `coaster_name` varchar(45) DEFAULT NULL,
+  `height(ft)` int DEFAULT NULL,
+  `speed(mph)` int DEFAULT NULL,
+  `Inversion_num` int DEFAULT NULL,
+  `year_opened` int DEFAULT NULL,
+  `year_closed` int DEFAULT NULL,
+  `location` varchar(45) DEFAULT NULL,
+  `park_ID` int DEFAULT NULL,
+  `comments` varchar(45) DEFAULT NULL,
+  `manufacturer` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`coasterID`),
+  KEY `park_ID_idx` (`park_ID`),
+  KEY `manufacturer_idx` (`manufacturer`),
+  CONSTRAINT `manufacturer` FOREIGN KEY (`manufacturer`) REFERENCES `manufacturer` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `park_ID` FOREIGN KEY (`park_ID`) REFERENCES `parks` (`parkID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `coasters`
+--
+
+LOCK TABLES `coasters` WRITE;
+/*!40000 ALTER TABLE `coasters` DISABLE KEYS */;
+/*!40000 ALTER TABLE `coasters` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manufacturer`
+--
+
+DROP TABLE IF EXISTS `manufacturer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `manufacturer` (
+  `name` varchar(45) NOT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `year_opened` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manufacturer`
+--
+
+LOCK TABLES `manufacturer` WRITE;
+/*!40000 ALTER TABLE `manufacturer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `manufacturer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `parks`
+--
+
+DROP TABLE IF EXISTS `parks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `parks` (
+  `parkID` int NOT NULL,
+  `park_name` varchar(30) DEFAULT NULL,
+  `location` varchar(45) DEFAULT NULL,
+  `year_opened` int DEFAULT NULL,
+  `num_passholders` int DEFAULT NULL,
+  `num_coasters` int DEFAULT NULL,
+  PRIMARY KEY (`parkID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `parks`
+--
+
+LOCK TABLES `parks` WRITE;
+/*!40000 ALTER TABLE `parks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `parks` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-10 14:02:19
