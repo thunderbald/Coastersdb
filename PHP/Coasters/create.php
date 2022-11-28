5fce3f22cb35c57a3ea636843afaf587e783c526
thunderbald <jasondsmith1@hotmail.com>
2022-11-28 15:00:54 -0500

    testing

--- PHP/Coasters/create.php
+++ PHP/Coasters/create.php
@@ -13,15 +13,15 @@ session_start();
 require_once "../config.php";
  
 // Define variables and initialize with empty values
-$coaster_name = $height = $speed = $coasterID = $Inversion_num = $year_opened  = $location = $parkID = $manufacturer = "";
-$coaster_name_err = $height_err = $speed_err = $coasterID_err = $Inversion_num_err = "";
+$coaster_name = $height = $speed = $coasterID =  $year_opened  = $location = $parkID = $manufacturer = "";
+$coaster_name_err = $height_err = $speed_err = $coasterID_err = "";
 $year_opened_err = $location_err = $parkID_err = $manufacturer_err =  "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate name
-    //$input_coaster_name = trim($_POST["coaster_name"]);
-    $input_coaster_name = trim(isset($_POST['coaster_name'])? $_POST['coaster_name'] : '');
+    $input_coaster_name = trim($_POST["coaster_name"]);
+    //$input_coaster_name = trim(isset($_POST['coaster_name'])? $_POST['coaster_name'] : '');
     if(empty($input_coaster_name)){
         $coaster_name_err = "Please enter a name.";
     } elseif(!filter_var($input_coaster_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
@@ -31,8 +31,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     //$reservation_id = isset($_POST['reservation_id']) ? $_POST['reservation_id'] : '';
     // Validate height
-    //$input_height = trim($_POST["height"]);
-    $input_height = trim(isset($_POST['height'])? $_POST['height'] : '');
+    $input_height = trim($_POST["height"]);
+    //$input_height = trim(isset($_POST['height'])? $_POST['height'] : '');
     if(empty($input_height)){
         $height_err = "Please enter coaster height.";
     } else{
@@ -40,8 +40,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     
     // Validate speed
-    //$input_speed = trim($_POST["speed"]);
-    $input_speed = trim(isset($_POST['speed'])? $_POST['speed'] : '');
+    $input_speed = trim($_POST["speed"]);
+    //$input_speed = trim(isset($_POST['speed'])? $_POST['speed'] : '');
     if(empty($input_speed)){
         $speed_err = "Please enter the speed.";
     } elseif(!ctype_digit($input_speed)){
@@ -50,8 +50,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $speed = $input_speed;
     }
     // Validate coasterID
-    //$input_coasterID = trim($_POST["coasterID"]);
-    $input_coasterID = trim(isset($_POST['coasterID'])? $_POST['coasterID'] : '');
+    $input_coasterID = trim($_POST["coasterID"]);
+    //$input_coasterID = trim(isset($_POST['coasterID'])? $_POST['coasterID'] : '');
     if(empty($input_coasterID)){
         $coasterID_err = "Please enter unique coasterID.";
     } else{
@@ -60,16 +60,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     // Validate number of inversions
     //$input_coasterID = trim($_POST["Inversion_num"]);
-    $input_Inversion_num = trim(isset($_POST['Inversion_num'])? $_POST['Inversion_num'] : '');
-    if(empty($input_Inversion_num)){
-        $Inversion_num_err = "Please enter number of inversions.";
-    } else{
-        $Inversion_num = $input_Inversion_num;
-    }
+    //$input_Inversion_num = trim(isset($_POST['Inversion_num'])? $_POST['Inversion_num'] : '');
+    //if(empty($input_Inversion_num)){
+      //  $Inversion_num_err = "Please enter number of inversions.";
+    //} //else{
+     //   $Inversion_num = $input_Inversion_num;
+    //}
 
     // Validate year opened
-    //$input_year_opened = trim($_POST["year_opened"]);
-    $input_year_opened = trim(isset($_POST['year_opened'])? $_POST['year_opened'] : '');
+    $input_year_opened = trim($_POST["year_opened"]);
+   // $input_year_opened = trim(isset($_POST['year_opened'])? $_POST['year_opened'] : '');
     if(empty($input_year_opened)){
         $year_opened_err = "Please enter the year opened.";
     } else{
@@ -77,24 +77,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 
     // Validate location
-    //$input_location = trim($_POST["location"]);
-    $input_location= trim(isset($_POST['location'])? $_POST['location'] : '');
+    $input_location = trim($_POST["location"]);
+    //$input_location= trim(isset($_POST['location'])? $_POST['location'] : '');
     if(empty($input_location)){
         $location_err = "Please enter the location.";
     } else{
         $location = $input_location;
     }
     // Validate parkID
-    //$input_parkID = trim($_POST["parkID"]);
-    $input_parkID = trim(isset($_POST['parkID'])? $_POST['parkID'] : '');
+    $input_parkID = trim($_POST["parkID"]);
+    //$input_parkID = trim(isset($_POST['parkID'])? $_POST['parkID'] : '');
     if(empty($input_parkID)){
         $parkID_err = "Please enter the parkID.";
     } else{
         $parkID = $input_parkID;
     }
     // Validate manufacturer
-    //$input_manufacturer = trim($_POST["manufacturer"]);
-    $input_manufacturer= trim(isset($_POST['manufacturer'])? $_POST['manufacturer'] : '');
+    $input_manufacturer = trim($_POST["manufacturer"]);
+    //$input_manufacturer= trim(isset($_POST['manufacturer'])? $_POST['manufacturer'] : '');
     if(empty($input_manufacturer)){
         $manufacturer_err = "Please enter the manufacturer.";
     } else{
@@ -105,20 +105,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($coaster_name_err) && empty($height_err) && empty($speed_err) && empty($coasterID_err)
         && empty($Inversion_num_err) && empty($year_opened_err) && empty($location_err) && empty($parkID_err) && empty($manufacturer_err)){
         // Prepare an insert statement
-        $sql = "INSERT INTO coasters (coasterID,  coaster_name, height, speed, Inversion_num, year_opened, location, parkID, manufacturer) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO coasters (coasterID,  coaster_name, height, speed, year_opened, location, parkID, manufacturer) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "siiiiisis", $param_name, $param_height,
-                $param_speed, $param_coasterID, $param_Inversion_num, $param_year_opened, $param_location, $param_parkID, $param_manufacturer);
+            mysqli_stmt_bind_param($stmt, "siiiisis", $param_name, $param_height,
+                $param_speed, $param_coasterID, $param_year_opened, $param_location, $param_parkID, $param_manufacturer);
 
             // Set parameters
             $param_name = $coaster_name;
             $param_height = $height;
             $param_speed = $speed;
             $param_coasterID = $coasterID;
-            $param_Inversion_num = $Inversion_num;
+            //$param_Inversion_num = $Inversion_num;
             $param_year_opened = $year_opened;
             $param_location = $location;
             $param_parkID = $parkID;
@@ -186,11 +186,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             <input type="text" name="speed" class="form-control <?php echo (!empty($speed_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $speed; ?>">
                             <span class="invalid-feedback"><?php echo $speed_err;?></span>
                         </div>
-                        <div class="form-group">
-                            <label>Number of Inversions</label>
-                            <input type="text" name="Inversion_num" class="form-control <?php echo (!empty($Inversion_num_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $Inversion_num; ?>">
-                            <span class="invalid-feedback"><?php echo $Inversion_num_err;?></span>
-                        </div>
+                       
                         <div class="form-group">
                             <label>Year Opened</label>
                             <input type="text" name="year_opened" class="form-control <?php echo (!empty($year_opened_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $year_opened; ?>">
