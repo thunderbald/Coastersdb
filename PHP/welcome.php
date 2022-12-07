c7b7f5bb02932922f9e1e582f23e9d2b27778b33
thunderbald <jasondsmith1@hotmail.com>
2022-12-07 09:26:16 -0500

    Updated welcome.

--- PHP/welcome.php
+++ PHP/welcome.php
@@ -24,7 +24,7 @@ session_start();
     </style>
 </head>
 <body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to the Rollercoasters Database!.</h1>
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
